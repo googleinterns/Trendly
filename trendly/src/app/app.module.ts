cbec1b817da81b75da10a8e81e7f605b54852c7a
Chen Raff <chenraff@google.com>
2020-09-22 09:37:26 +0000

    Change app module to include ClusterlyComponent

--- trendly/src/app/app.module.ts
+++ trendly/src/app/app.module.ts
@@ -1,20 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router'
-import { TopBarComponent } from './top-bar/top-bar.component';
-
 import {MatIconModule} from '@angular/material/icon';
 
+import { TopBarComponent } from './top-bar/top-bar.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainPageComponent } from './main-page/main-page.component';
-// import { ClusterlyComponent } from './clusterly/clusterly.component';
+import { ClusterlyComponent } from './clusterly/clusterly.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainPageComponent,
     TopBarComponent,
+    ClusterlyComponent
   ],
   imports: [
     BrowserModule,
@@ -23,7 +23,7 @@ import { MainPageComponent } from './main-page/main-page.component';
     RouterModule.forRoot([
       { path: '', component: MainPageComponent },
       { path: 'histogramy', component: MainPageComponent },
-      { path: 'clusterly', component: MainPageComponent }
+      { path: 'clusterly', component: ClusterlyComponent }
     ])
   ],
   providers: [],
