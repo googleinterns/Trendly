846dfb32697719fe530eec1fa8c1153c2c0723dd
Chen Raff <chenraff@google.com>
2020-09-23 13:08:20 +0000

    Add data types classes used by Google Trends API

---
+++ trendly/src/backend/TrendsQuery.java
@@ -0,0 +1,5 @@
+/** Google Trends API query data type */
+public class TrendsQuery {
+  public String title;
+  public double value;
+}
---
+++ trendly/src/backend/TrendsTopicsResult.java
@@ -0,0 +1,4 @@
+/** Google Trends API top topic result data type */
+public class TrendsTopicsResult implements TrendsResult {
+  public TrendsTopic[] item;
+}
