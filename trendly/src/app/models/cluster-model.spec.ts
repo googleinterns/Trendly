c759c7471bb6d75845e9ba27b9b3c30d3277b34c
Chen Raff <chenraff@google.com>
2020-09-24 10:31:39 +0000

    Add get2ClustersAndBubble func

--- trendly/src/app/models/cluster-model.spec.ts
+++ trendly/src/app/models/cluster-model.spec.ts
@@ -1,22 +1,21 @@
 import { Cluster } from './cluster-model';
 
+const CLUSTER = new Cluster("", 1,
+    [{ queryString: "", volume: 15 },
+    { queryString: "", volume: 14 },
+    { queryString: "", volume: 13 }]);
+
 describe('Cluster', () => {
     /** Checks the bubbles set property contains the same amount of
      * queries as the given query data */
     it('check bubbles property size', () => {
-        const cluster = new Cluster("curr", 1,
-            [{ queryString: "", volume: 15 },
-            { queryString: "", volume: 14 },
-            { queryString: "", volume: 13 }]);
+        const cluster = CLUSTER;
         expect(cluster.bubbles.size).toBe(3);
     });
 
     /** Checks the bubbles were created with the correct cluster id */
     it('check bubbles cluster id', () => {
-        const cluster = new Cluster("curr", 1,
-            [{ queryString: "", volume: 15 },
-            { queryString: "", volume: 14 },
-            { queryString: "", volume: 13 }]);
+        const cluster = CLUSTER;
         cluster.bubbles.forEach((bubble) => {
             expect(bubble.clusterId).toBe(1);
         })
@@ -25,9 +24,7 @@ describe('Cluster', () => {
     /** Checks moveBubbleToAnotherCluster correctly remove the given bubble
      * from the current cluster its called on */
     it('check moveBubbleToAnotherCluster removes bubble from current cluster', () => {
-        const currCluster = new Cluster("curr", 1, [{ queryString: "", volume: 15 }]);
-        const newCluster = new Cluster("curr", 2, [{ queryString: "", volume: 100 }]);
-        const bubble = currCluster.bubbles[0];
+        const [currCluster, newCluster, bubble] = get2ClustersAndBubble();
         currCluster.moveBubbleToAnotherCluster(bubble, newCluster);
         expect(currCluster.bubbles.has(bubble)).toBeFalse();
     });
@@ -35,12 +32,18 @@ describe('Cluster', () => {
     /** Checks moveBubbleToAnotherCluster correctly adds the given bubble
      * to the other cluster its given */
     it('check moveBubbleToAnotherCluster adds bubble to the given cluster', () => {
-        const currCluster = new Cluster("curr", 1, [{ queryString: "", volume: 15 }]);
-        const newCluster = new Cluster("curr", 2, [{ queryString: "", volume: 100 }]);
-        const bubble = currCluster.bubbles[0];
+        const [currCluster, newCluster, bubble] = get2ClustersAndBubble();
         expect(newCluster.bubbles.has(bubble)).toBeFalse();
         currCluster.moveBubbleToAnotherCluster(bubble, newCluster);
         expect(newCluster.bubbles.has(bubble)).toBeTrue();
     });
-
 });
+/** Helper function for moveBubbleToAnotherCluster tests.
+ *  Returns 2 created clusters and a bubble that belongs to the first one.
+ */
+function get2ClustersAndBubble() {
+    const currCluster = new Cluster("", 1, [{ queryString: "", volume: 15 }]);
+    const newCluster = new Cluster("", 2, [{ queryString: "", volume: 100 }]);
+    const bubble = currCluster.bubbles[0];
+    return [currCluster, newCluster, bubble];
+}
