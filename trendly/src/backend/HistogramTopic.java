1a2a435445c253951b880cc84116c9da7ce96b21
Adi Assulin <adiassu@google.com>
2020-10-04 07:01:25 +0000

    adds servlets and backend classes

---
+++ trendly/src/backend/HistogramTopic.java
@@ -0,0 +1,11 @@
+/**
+ * Represents Trends Histogram topic which contains trends topic properties +
+ *  description.
+ */
+public class HistogramTopic extends TrendsTopic {
+  public String description;
+  @Override
+  public String toString() {
+    return this.title + this.mid + this.value + this.description;
+  }
+}
---
+++ trendly/src/backend/TrendsHistogramResult.java
@@ -0,0 +1,6 @@
+/**
+ * An Interface represents an histogram data result.
+ */
+public interface TrendsHistogramResult extends TrendsResult {
+  public TrendsTopic[] getItem();
+}
