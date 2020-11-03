71af399c3b89bc91bebdf578bad33d8acd1ec064
Adi Assulin <adiassu@google.com>
2020-11-03 07:22:39 +0000

    update master toggle and UI changes

--- trendly/src/app/clusterly/clusterly.component.ts
+++ trendly/src/app/clusterly/clusterly.component.ts
@@ -2,13 +2,13 @@ import {SelectionModel} from '@angular/cdk/collections';
 import {FlatTreeControl} from '@angular/cdk/tree';
 import {Component, OnChanges, SimpleChanges} from '@angular/core';
 import {Injectable} from '@angular/core';
+import {MatTableDataSource} from '@angular/material/table';
 import {MatTreeFlatDataSource, MatTreeFlattener} from '@angular/material/tree';
 import {BehaviorSubject, Observable} from 'rxjs';
 
 import {InputObj} from '../clusterly-inputs/clusterly-inputs.component';
 import {DataService} from '../data.service';
 import {Cluster} from '../models/cluster-model';
-import {MatTableDataSource} from '@angular/material/table';
 
 /**
  * Clusterly component - includes Clusterly inputs & clusters-section
@@ -27,21 +27,23 @@ export class ClusterlyComponent {
   clusters: Map<number, Cluster> = new Map<number, Cluster>();
   clustersToShow: Map<number, Cluster>;
   displayedColumns: string[] = ['select', 'title', 'volume'];
-  dataSource : MatTableDataSource<Cluster>;
+  dataSource: MatTableDataSource<Cluster>;
   selection = new SelectionModel<Cluster>(true, []);
-
-  private selectedID: Set<number> = new Set<number>();
-
-
+  
   constructor(private dataService: DataService) {
     this.clustersToShow = new Map<number, Cluster>();
-    this.dataSource = new MatTableDataSource<Cluster>([...this.clusters.values()]);
+    this.dataSource =
+        new MatTableDataSource<Cluster>([...this.clusters.values()]);
   }
 
+  /**
+   * Changes side-nav view according changes that done in the UI.
+   */
   changeView(): void {
     const sortedClusters: Map<number, Cluster> = new Map(
         [...this.clusters.entries()].sort((a, b) => b[1].volume - a[1].volume));
-    this.dataSource = new MatTableDataSource<Cluster>([...sortedClusters.values()]);
+    this.dataSource =
+        new MatTableDataSource<Cluster>([...sortedClusters.values()]);
   }
 
   getDataFromServer(input: InputObj) {
@@ -62,30 +64,40 @@ export class ClusterlyComponent {
             });
   }
 
+  /**
+   * Change the row selection when clicked and accordingly updates the clusters to show.
+   */
   ItemToggle(row) {
     this.selection.toggle(row);
     this.updateClustersToShow();
   }
 
-  private updateClustersToShow() : void {
+  /**
+   * Updates clusters to show according the selections.
+   */
+  private updateClustersToShow(): void {
     this.clustersToShow = new Map<number, Cluster>();
     this.selection.selected.forEach((cluster) => {
       this.clustersToShow.set(cluster.id, cluster);
     })
   }
 
-  /** Whether the number of selected elements matches the total number of rows. */
+  /**
+   * Whether the number of selected elements matches the total number of rows.
+   */
   isAllSelected() {
     const numSelected = this.selection.selected.length;
     const numRows = this.dataSource.data.length;
     return numSelected === numRows;
   }
 
-  /** Selects all rows if they are not all selected; otherwise clear selection. */
+  /**
+   * Selects all rows if they are not all selected; otherwise clear selection.
+   */
   masterToggle() {
     this.isAllSelected() ?
         this.selection.clear() :
         this.dataSource.data.forEach((row) => this.selection.select(row));
+    this.updateClustersToShow();
   }
-
 }
